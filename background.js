4bedaeddd0ee5ca462271a56092204ef838aa2af
Telenchak Oleksiy <letel315@gmail.com>
2024-09-10 16:07:23 +0300

    Add logic

--- background.js
+++ background.js
@@ -1,18 +1,30 @@
 const saveTabs = (name) => {
-  chrome.tabs.query({ currentWindow: true }, (tabs) => {
+  chrome.tabs.query({ currentWindow: true }, function (tabs) {
     const urls = tabs.map((tab) => tab.url);
-    chrome.storage.local.set({ [name]: urls });
+    chrome.storage.local.set({ [name]: urls }, function () {
+      console.log("Tabs saved under the name:", name);
+    });
   });
 };
 
-const openTabs = () => {
-  chrome.storage.local.get((result) => {});
+const openTabs = (name) => {
+  chrome.storage.local.get([name], function (result) {
+    const urls = result[name];
+    if (urls) {
+      urls.forEach((url) => chrome.tabs.create({ url }));
+    }
+  });
+};
+
+const getTabs = () => {
+  chrome.storage.local.get();
 };
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "saveTabs") {
     saveTabs(request.name);
   } else if (request.action === "openTabs") {
-    openTabs();
+    openTabs(request.name);
   }
+  sendResponse({ status: "done" });
 });
--- popup.js
+++ popup.js
@@ -1,11 +1,36 @@
 const nameInput = document.getElementById("save-name");
 const saveTabsBtn = document.getElementById("save-tabs-btn");
 const openTabsBtn = document.getElementById("open-tabs-btn");
-
+const tabsList = document.getElementById("open-tab-set-list");
+const tabsListIsVisible = false;
 saveTabsBtn.addEventListener("click", () => {
   const name = nameInput.value;
   if (name) {
+    chrome.runtime.sendMessage(
+      { action: "saveTabs", name: name },
+      function (response) {
+        console.log("Tabs saved:", response);
+      }
+    );
   }
 });
 
-openTabsBtn.addEventListener("click", () => {});
+openTabsBtn.addEventListener("click", () => {
+  tabsListIsVisible = !tabsListIsVisible;
+  if (tabsListIsVisible) {
+    openTabsBtn.innerHTML = "Hide Tab Sets";
+    tabsList.style.display = "block";
+  } else {
+    openTabsBtn.innerHTML = "Show Tab Sets";
+    tabsList.style.display = "none";
+  }
+  const name = nameInput.value;
+  if (name) {
+    chrome.runtime.sendMessage(
+      { action: "openTabs", name: name },
+      function (response) {
+        console.log("Tabs saved:", response);
+      }
+    );
+  }
+});
